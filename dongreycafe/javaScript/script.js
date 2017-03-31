cddf87eebe8429754d75858685f28cdc4a0012c7
laurakurtzberg <laura.kurtzberg@gmail.com>
2017-03-31 12:12:59 -0400

    new video don grey

--- dongreycafe/javaScript/script.js
+++ dongreycafe/javaScript/script.js
@@ -1,3 +1,10 @@
+document.getElementById("downarrow").onclick = function() {myScrollFunction()};
+
+function myScrollFunction() {
+        var element_to_scroll_to = document.getElementById('demovideo');
+        element_to_scroll_to.scrollIntoView({block: "end", behavior: "smooth"});
+};
+
 $(window).scroll(function() {
 
 	var windowScroll = $(this).scrollTop();
