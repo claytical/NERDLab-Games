1af459340892ee7695010bcfdef7c44cce225b42
laurakurtzberg <laura.kurtzberg@gmail.com>
2017-03-31 13:22:01 -0400

    template starter

---
+++ micrositeTemplate/javaScript/script.js
@@ -0,0 +1,24 @@
+document.getElementById("downarrow").onclick = function() {myScrollFunction()};
+
+function myScrollFunction() {
+        var element_to_scroll_to = document.getElementById('demovideo');
+        element_to_scroll_to.scrollIntoView({block: "end", behavior: "smooth"});
+};
+
+$(window).scroll(function() {
+
+	var windowScroll = $(this).scrollTop();
+
+	var  windowWidth = $(window).width();
+
+
+  if (windowWidth >= 500) {
+
+    if (windowScroll > 120) {
+      $('.learnMore').fadeOut();
+    }
+    else {
+      $('.learnMore').fadeIn();
+    }
+  }
+});
