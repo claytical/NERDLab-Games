920e25e822895d1755700e54471562a8947da454
laurakurtzberg <laura.kurtzberg@gmail.com>
2017-10-20 17:27:16 -0400

    new vanity folder and new UM folder

---
+++ vanity/javaScript/script.js
@@ -0,0 +1,26 @@
+document.getElementById("downarrow").onclick = function() {myScrollFunction()};
+
+function myScrollFunction() {
+        var element_to_scroll_to = document.getElementById('scrollhere');
+        element_to_scroll_to.scrollIntoView({block: "end", behavior: "smooth"});
+};
+
+$(window).scroll(function() {
+
+	var windowScroll = $(this).scrollTop();
+
+	var  windowWidth = $(window).width();
+
+
+  if (windowWidth >= 500) {
+
+    if (windowScroll > 120) {
+      $('.learnMore').fadeOut();
+    }
+    else {
+      $('.learnMore').fadeIn();
+    }
+  }
+});
+
+document.getElementById("uparrow").onclick = function() {$('html, body').animate({ scrollTop: 0 }, 'fast');};
